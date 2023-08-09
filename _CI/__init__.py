cf5113508daacfc0498e59eba67ed75720b2bf1f
Costas Tyfoxylos <costas.tyf@gmail.com>
2023-08-09 12:17:58 +0200

    Expose useful variables and methods under _CI.

---
+++ _CI/__init__.py
@@ -0,0 +1,14 @@
+from _CI.lib.tasks.configuration import (CURRENT_SHELL,
+                                         EMOJI_FAILURE_PREFIX,
+                                         EMOJI_FAILURE_SUFFIX,
+                                         EMOJI_SUCCESS_PREFIX,
+                                         EMOJI_SUCCESS_SUFFIX,
+                                         PROJECT_ROOT_DIRECTORY,
+                                         INVOKE_LOGGING_LEVEL)
+from _CI.lib.tasks.utils import (delete_file_or_directory,
+                                 download_with_progress_bar,
+                                 emojize_message,
+                                 get_binary_path,
+                                 make_file_executable,
+                                 pushd,
+                                 validate_log_level)
